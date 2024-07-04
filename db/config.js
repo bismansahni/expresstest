2e1b58753bb61a34b9bbfaea738168df5ba7c84f
bismansahni <bismansahni@outlook.com>
2024-07-05 00:30:21 +0530

    better connection pathway for mongodb

--- db/config.js
+++ db/config.js
@@ -1,22 +1,17 @@
 
-
-
-import { config } from 'dotenv';
-config();
-
 import mongoose from 'mongoose';
 
-const uri = process.env.MONGO_URI;
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+     
+     
+    });
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection failed', error);
+    process.exit(1);
+  }
+};
 
-if (!uri) {
-  throw new Error("MONGO_URI is not defined in the environment variables.");
-}
-
-mongoose.connect(uri)
-  .then(() => {
-    console.log("Successfully connected to MongoDB!");
-  })
-  .catch(err => {
-    console.error("Connection error", err);
-    process.exit();
-  });
+export default connectDB;
