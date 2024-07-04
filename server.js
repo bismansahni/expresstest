f787e3122eb944f6583ee089e0cd17317443b263
bismansahni <bismansahni@outlook.com>
2024-07-04 21:21:24 +0530

    using cors

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors'; 
 import authRoutes from './routes/auth.js';
 import './db/config.js';
 
 dotenv.config();
 
+app.use(cors());  // Enable CORS
+app.use(express.json());
+
 const app = express();
 const port = process.env.PORT || 5000;
+
 
 
 app.use(express.json());
